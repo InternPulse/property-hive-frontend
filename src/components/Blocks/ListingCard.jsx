f06770b8e17a7dbdc4792246717eb9cdf97ec923
Roland Joshua <289volts@gmail.com>
2024-09-21 23:12:10 +0100

    test: Implemented final test for CustomDropDown logic

--- src/components/Blocks/ListingCard.jsx
+++ src/components/Blocks/ListingCard.jsx
@@ -3,13 +3,21 @@ import PropTypes from "prop-types";
 const ListingCard = ({ title, location, details, price, image }) => {
   const modDetails = details.join(" | ");
   return (
-    <div className="space-y-2 rounded-lg bg-neutrals-25 p-4">
+    <div
+      data-testid="property-listing-card"
+      className="space-y-2 rounded-lg bg-neutrals-25 p-4"
+    >
       <div className="rounded-lg">
         <img src={image} alt="" className="" />
       </div>
       <div className="">
         <div className="space-y-0.5">
-          <h2 className="text-lg font-medium text-neutrals-950">{title}</h2>
+          <h2
+            data-testid="property-listing-card-title"
+            className="text-lg font-medium text-neutrals-950"
+          >
+            {title}
+          </h2>
           <p className="font-medium text-neutrals-700">{location}</p>
           <p className="text-sm font-medium text-neutrals-700">{modDetails}</p>
           <p className="text-xl font-semibold text-primary-800">{price}</p>
