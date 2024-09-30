c3bac041ae36f50e5e0745e0fa6749417241d0b8
MakZwane71 <makhohlisathemba@gmail.com>
2024-09-30 10:25:59 +0200

    fixed footer margins

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -4,22 +4,25 @@ import ContactUs from "./ContactUs/ContactUs";
 import Subscription from "./NewsLetter/Subscription";
 import Features from "../../../components/home/Features";
 import Testimonial from "../../../components/home/carousel/Testimonial";
+import Footer from "../../../components/home/footer/footer";
 
-
-
-
-const Home = () => { 
+const Home = () => {
   return (
     <>
       <HeroSection />
-      <Features />
-    <Testimonial />
+      <section id="features">
+        <Features />
+      </section>
+      <section id="testimonal">
+        <Testimonial />
+      </section>
       <section id="contact">
         <ContactUs />
       </section>
       <Subscription />
+      <Footer />
     </>
   );
 };
 
-export default Home
+export default Home;
